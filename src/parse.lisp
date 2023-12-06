aaaa0ecc661de6a5da3b130fea9a387c9adb3d02
Arda K <ardakocer@live.de>
2023-12-06 15:16:38 +0100

    feat(parse): decode timestamps

--- src/parse.lisp
+++ src/parse.lisp
@@ -16,16 +16,36 @@
       nil))
 
 (defun getStartTime (line)
-  (subseq line (+ (position #\: line :test #'equal) 1)))
+  (decodeTimestamp (subseq line (+ (position #\: line :test #'equal) 1))))
 
 (defun getEndTime (line)
   (cond ((search "DTEND" line) (progn
-				 (subseq line (+ (position #\: line :test #'equal) 1))))
+				 (decodeTimestamp (subseq line (+ (position #\: line :test #'equal) 1)))))
 	((search "DURATION" line) (progn
-				    (subseq line (+ (position #\: line :test #'equal) 1))))
+				    (decodeTimestamp (subseq line (+ (position #\: line :test #'equal) 1)))))
 	(t (format t "FAILURE, NO DTEND OR DURATION FOUND."))))
 
-(defun calcEndTime (startTime, duration))
+(defun calcEndTime (startTime duration))
   
 
-(defun decodeTimestamp (timestamp))
+(defun decodeTimestamp (timestamp)
+  (let ((year (subseq timestamp 0 4))
+	(month (subseq timestamp 4 6))
+	(day (subseq timestamp 6 8))
+	(hour nil)
+	(minute nil)
+	(second nil)
+	(outputString nil))
+    (progn
+      ;; find out if timestamp is only date or has time attached to it
+      (if (search "T" timestamp)
+	  (progn
+	    (setf hour (subseq timestamp 9 11))
+	    (setf minute (subseq timestamp 11 13))
+	    (setf second (subseq timestamp 13 15))
+	    (setf outputString (format NIL "~a.~a.~a ~a:~a:~a" day month year hour minute second))
+	    (format t "Decoded timestamp (dd.mm.yyyy HH:MM:SS) ~a~%" outputString))
+	  (progn
+	    (setf outputString (format NIL "~a.~a.~a" day month year))
+	    (format t "Decoded timestamp (dd.mm.yyyy) ~a~%" outputString))))
+    outputString))
