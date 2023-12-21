d2435e8077221404ee034ad63f0971749c126c8c
suptower <70473844+suptower@users.noreply.github.com>
2023-12-21 16:53:01 +0100

    feat(inspect): added detailed flag

--- src/inspect.lisp
+++ src/inspect.lisp
@@ -8,10 +8,17 @@
     :long-name "file"
     :description "(REQUIRED) The ical file to inspect"
     :key :file
-    :required t)))
+    :required t)
+    (clingon:make-option
+    :flag
+    :short-name #\d
+    :long-name "detailed"
+    :description "Show detailed description of events"
+    :key :detail)))
 
 (defun inspect/handler (cmd)
-  (let ((file (clingon:getopt cmd :file)))))
+  (let ((file (clingon:getopt cmd :file))
+        (detail (clingon:getopt cmd :detail)))))
 
 (defun inspect/command ()
   (clingon:make-command
